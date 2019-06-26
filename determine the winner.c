bba242e7b775d5601d1de4fefe649df0e4dba5bb
Aashish kumar singh <41873953+ash-123-ish@users.noreply.github.com>
2019-06-26 22:26:45 +0530

    Add files via upload

---
+++ determine the winner.c
@@ -0,0 +1,65 @@
+
+#include <stdio.h>
+
+#include<math.h>
+
+
+int main(){
+
+	int testcase;
+
+	float n;
+	
+	float k;
+
+	scanf("%d", &testcase);
+
+	if(testcase<=100000)
+
+	{
+
+	for(int i=0;i<testcase;i++)
+
+	{
+
+	    scanf("%d",&n);
+
+	    scanf("%d",&k);
+
+	    float c=n/k;
+
+	    int d=ceil(c);
+
+	    //if(1.000<=k && k<=n && n<=100000.000)
+
+	    if(d%2==0)
+
+	    {
+
+	        printf("Dishant\n");
+
+	    }
+
+	    else
+
+	    {
+
+	        printf("Arpa\n");
+
+	    }
+
+	}
+
+	}
+
+	else
+
+	{
+
+	    return 0;
+
+	}
+
+
+}
+
