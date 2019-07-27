8e9c492cbab82a843393237aa32f6ab8d85fc84e
Aashish kumar singh <41873953+ash-123-ish@users.noreply.github.com>
2019-07-27 11:05:24 +0530

    Add files via upload

---
+++ abhishek like to play with numbers answer.py
@@ -0,0 +1,43 @@
+n,m=list(map(int,input().split(" ")))
+
+l=list(map(int,input().split(" ")))
+
+d={}
+
+for i in l:
+
+    c=abs(m-i)
+
+    try:d[c].append(i)
+
+    except:d[c]=[i]
+
+f=list(d.keys())
+
+f.sort()
+
+c=0
+
+fl=0
+
+for i in f:
+
+    k=d[i]
+
+    k.sort(reverse=True)
+
+    for j in k:
+
+        print(j,end=" ")
+
+        c+=1
+
+        if(c==5):
+
+            fl=5
+
+            break
+
+    if(fl==5):
+
+        break
